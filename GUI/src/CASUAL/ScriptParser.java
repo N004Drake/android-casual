93e5454f634b0484331d8cd3a8e7e8c0ae51335c
adamoutler <adamoutler@gmail.com>
2012-02-12 16:45:16 +0000

    Initial commit of ScriptParser.  Acknowledges data sent to it.

---
+++ GUI/src/CASUAL/ScriptParser.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CASUAL;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author adam
+ */
+public class ScriptParser {
+
+    private static void parseScript() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+    Log Log=new Log();
+    
+    
+    public void executeSelectedScript(String Script){
+        Log.level1("Selected "+ Script + " " );
+        //TODO open as datastream and pass to DoRead
+    }
+    
+    
+    private static void DoRead(DataInputStream dataIn) {
+        try {
+            BufferedReader bReader = new BufferedReader(new InputStreamReader(dataIn));
+            String strLine;
+            while ((strLine = bReader.readLine()) != null) {
+                parseScript();
+            }
+            //Close the input stream
+            dataIn.close();
+        } catch (Exception e) {//Catch exception if any
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+    
+    
+}
