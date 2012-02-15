e1379e5a5804ec5df0db087f86b44c9f101c67ff
adamoutler <adamoutler@gmail.com>
2012-02-15 00:09:43 +0000

    added diffInputStreamVersusFile

--- GUI/src/CASUAL/diffTextFiles.java
+++ GUI/src/CASUAL/diffTextFiles.java
@@ -14,10 +14,31 @@ import java.util.logging.Logger;
  */
 public class diffTextFiles {
 
+    public String diffInputStreamVersusFile(InputStream TestIStream, String OriginalFile) throws IOException {
+        String Difference = "";
+        BufferedReader TestStream = new BufferedReader(new InputStreamReader(TestIStream));
+        File Original = new File(OriginalFile);
+        String TestStreamLine;
+        String OriginalFileLine;
+        while ((TestStreamLine = TestStream.readLine()) != null) {
+            boolean LineExists = false;
+            BufferedReader OriginalReader = new BufferedReader(new FileReader(Original));
+            while ((OriginalFileLine = OriginalReader.readLine()) != null) {
+                if (OriginalFileLine.equals(TestStreamLine)) {
+                    LineExists = true;
+                }
+            }
+            if (!LineExists) {
+                Difference = Difference + "\n" + TestStreamLine;
+            }
+        }
+        return Difference;
 
-   public String diffTextFiles(String File1, String File2) {
-                   String DifferenceFromFile1 = "";
-       try {
+    }
+
+    public String diffTextFiles(String File1, String File2) {
+        String DifferenceFromFile1 = "";
+        try {
             BufferedReader Reader1 = new BufferedReader(new FileReader(File1));
             try {
 
@@ -42,44 +63,41 @@ public class diffTextFiles {
                         Reader2.close();
                     }
                 }
-        } finally {
-            Reader1.close();
-        }
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-       return DifferenceFromFile1;
-}
-   //Takes in the Diff from the constructor and writes it to the file that is 
-   //iniFile.
-   private void writeDiffToIni(String diff){
-   InputStream ResourceAsStream = getClass().getResourceAsStream(Statics.ADBini);
-   try{
-   BufferedReader d =new BufferedReader(new InputStreamReader(ResourceAsStream));
-        try {
-            while((currentString = d.readLine()) != null)
-            {
-            FileOutputStream fout = new FileOutputStream(Statics.ADBini + "_new"); 
-            new PrintStream(fout).println(currentString);
+            } finally {
+                Reader1.close();
             }
-            new PrintStream(fout).println(diff);
-        } 
-        finally{
-            fout.close();
-            d.close();
-            File file = new File(Statics.ADBini);
-            file.delete();
-            file = new File(Statics.ADBini + "_new");
-            file.renameTo(new File(Statics.ADBini).getAbsoluteFile());
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-   }
-        catch (IOException ex) {
+        return DifferenceFromFile1;
+    }
+    //Takes in the Diff from the constructor and writes it to the file that is 
+    //iniFile.
+
+    private void writeDiffToIni(String diff) {
+        InputStream ResourceAsStream = getClass().getResourceAsStream(Statics.ADBini);
+        try {
+            BufferedReader d = new BufferedReader(new InputStreamReader(ResourceAsStream));
+            try {
+                while ((currentString = d.readLine()) != null) {
+                    FileOutputStream fout = new FileOutputStream(Statics.ADBini + "_new");
+                    new PrintStream(fout).println(currentString);
+                }
+                new PrintStream(fout).println(diff);
+            } finally {
+                fout.close();
+                d.close();
+                File file = new File(Statics.ADBini);
+                file.delete();
+                file = new File(Statics.ADBini + "_new");
+                file.renameTo(new File(Statics.ADBini).getAbsoluteFile());
+            }
+        } catch (IOException ex) {
             Logger.getLogger(diffTextFiles.class.getName()).log(Level.SEVERE, null, ex);
         }
-   }
-   
-private StringBuilder string1, string2;
-private String currentString;
-private BufferedReader d;
-private FileOutputStream fout;
+    }
+    private StringBuilder string1, string2;
+    private String currentString;
+    private BufferedReader d;
+    private FileOutputStream fout;
 }
