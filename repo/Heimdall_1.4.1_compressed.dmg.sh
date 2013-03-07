946480785f58fa16a6ac5afcaac3c59a392da148
adamoutler <adamoutler@gmail.com>
2013-03-07 00:07:57 +0000

    Adding control script to be downloaded and executed on mac

---
+++ repo/Heimdall_1.4.1_compressed.dmg.sh
@@ -0,0 +1,3 @@
+curl http://android-casual.googlecode.com/svn/trunk/repo/Heimdall_1.4.1_compressed.dmg>./heimdall.dmg
+hdiutil mount heimdall.dmg
+open /volumes/Heimdall_1.4.1/Heimdall\ Suite\ 1.4.1.pkg
