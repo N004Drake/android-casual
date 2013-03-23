f13e36d76d5bbf239e01a00be46ed84d500e3bad
adamoutler <adamoutler@gmail.com>
2013-03-23 17:57:47 +0000

    updating Heimdall-mac installer

--- repo/Heimdall_1.4.1_compressed.dmg.sh
+++ repo/Heimdall_1.4.1_compressed.dmg.sh
@@ -1,8 +1,8 @@
 curl http://android-casual.googlecode.com/svn/trunk/repo/Heimdall_1.4.1_compressed.dmg>./heimdall.dmg
 hdiutil mount heimdall.dmg
 open /volumes/Heimdall_1.4.1/Heimdall\ Suite\ 1.4.1.pkg
-#hdiutil umount heimdall.dmg
-#rm ./heimdall.dmg
-#sudo chown -R root:wheel /System/Library/Extensions/heimdall.kext
-#sudo kextload /System/Library/Extensions/heimdall.kext
+hdiutil umount heimdall.dmg
+rm ./heimdall.dmg
+sudo chown -R root:wheel /System/Library/Extensions/heimdall.kext
+sudo kextload /System/Library/Extensions/heimdall.kext
 #Either restart USB somehow or figure out how to 
