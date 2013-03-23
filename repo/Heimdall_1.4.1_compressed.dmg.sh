64c102554576a27dd7b2e010ff731c60ab779ce4
adamoutler <adamoutler@gmail.com>
2013-03-23 23:23:10 +0000

    make heimdall blocking for post-install kexts

--- repo/Heimdall_1.4.1_compressed.dmg.sh
+++ repo/Heimdall_1.4.1_compressed.dmg.sh
@@ -1,6 +1,6 @@
 curl http://android-casual.googlecode.com/svn/trunk/repo/Heimdall_1.4.1_compressed.dmg>./heimdall.dmg
 hdiutil mount heimdall.dmg
-sudo open /volumes/Heimdall_1.4.1/Heimdall\ Suite\ 1.4.1.pkg
+sudo open -W /volumes/Heimdall_1.4.1/Heimdall\ Suite\ 1.4.1.pkg
 hdiutil umount heimdall.dmg
 rm ./heimdall.dmg
 sudo chown -R root:wheel /System/Library/Extensions/heimdall.kext
