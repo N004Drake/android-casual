c2cd9ad07d91bac489bf28089515df176b08a2d1
adamoutler <adamoutler@gmail.com>
2013-04-22 20:31:50 +0000

    Better input for CASUALInteraction

--- GUI/src/CASUAL/CASUALInteraction.java
+++ GUI/src/CASUAL/CASUALInteraction.java
@@ -20,11 +20,7 @@ import java.awt.Component;
 import java.awt.HeadlessException;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 /**
@@ -32,6 +28,7 @@ import javax.swing.JOptionPane;
  * @author adam
  */
 public class CASUALInteraction extends JOptionPane {
+    public static BufferedReader in =new BufferedReader(new InputStreamReader(System.in));
 
     public int showTimeoutDialog(final int PRESET_TIME, Component parentComponent, Object message, final String title, int optionType, int messageType, Object[] options, final Object initialValue) {
         if (Statics.useGUI) {
@@ -46,11 +43,7 @@ public class CASUALInteraction extends JOptionPane {
 
     public String getCommandLineInput() {
         try {
-            String input;
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
             return in.readLine();
-                
-                
         } catch (IOException ex) {
             return "";
         }
