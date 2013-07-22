47cf372702b6874e5e0c35067a593e1858619283
loglud <loglud@logatech.org>
2013-07-22 11:11:22 +0000

    Added JavaDocs to all public methods in Unzip.java

--- CASUALcore/src/CASUAL/Unzip.java
+++ CASUALcore/src/CASUAL/Unzip.java
@@ -38,13 +38,20 @@ public class Unzip {
 
     static int BUFFER = 4096;
     ZipFile zip;
+    /**
+     *
+     */
     public Enumeration zipFileEntries;
 
     /**
-     * unzip unzips files
-     * @param f file to be unzip
+     * Unzip class is used to create a wrapper for unziping .zip files.
+     * <p>
+     * The File f will be converted to a ZipFile, and all other operations will
+     * be preformed on this ZipFile.
+     * @param f java file object to be unziped.
      * @throws ZipException
      * @throws IOException
+     * @see ZipFile
      */
     public Unzip(File f) throws ZipException, IOException {
         this.zip = new ZipFile(f);
@@ -56,10 +63,14 @@ public class Unzip {
     }
 
     /**
-     * unzip unzips files
-     * @param f file to unzip
+     * Unzip class is used to create a wrapper for unziping .zip files.
+     * <p>
+     * The String f will be converted into a file, then that file will be 
+     * converted into a ZipFile.
+     * @param f String location of file to be unziped.
      * @throws ZipException
      * @throws IOException
+     * @see ZipFile
      */
     public Unzip(String f) throws ZipException, IOException {
         this.zip = new ZipFile(new File(f));
@@ -71,8 +82,10 @@ public class Unzip {
     }
 
     /**
-     * unzips recursively
-     * @param outputFolder to folder
+     * Unzips the current file, and then if any zip files are found within the
+     * zip, they are then unziped as a recursive function.
+     * @deprecated As of now method is not functioning nor needed
+     * @param outputFolder String of folder location to output to.
      * @throws ZipException
      * @throws IOException
      */
@@ -106,10 +119,11 @@ public class Unzip {
     }
 
     /**
-     * unzips a file to a folder
+     * Unzips the ZipFile that was specified in the constructor of the class.
      * @param outputFolder folder to be unzipped to
      * @throws ZipException
      * @throws IOException
+     * @see CASUAL.Unzip#Unzip(File) 
      */
     public void unzipFile(String outputFolder) throws ZipException, IOException {
         unzipFileToFolder(outputFolder);
@@ -142,28 +156,44 @@ public class Unzip {
     }
 
     /**
-     * unzips a resource
-     * @param zipResource resource to be unzipped
+     * Unzips a resource.
+     * <p>
+     * Within a java package there is a folder called resources, used to store things
+     * such as internalized strings, sounds, and other important static files. This function
+     * takes in the name of the resource and then outputs it into the output folder.
+     * @param zipResource name of the java resource to be unzipped
      * @param outputFolder folder to unzip to
      * @throws FileNotFoundException
      * @throws IOException
+     * @see java.lang.Class#getResource(String) 
+     * 
      */
     public static void unZipResource(String zipResource, String outputFolder) throws FileNotFoundException, IOException {
-        InputStream ZStream = new CASUALApp().getClass().getResourceAsStream(zipResource);
-        unZipInputStream(ZStream, outputFolder);
+        InputStream zStream;
+        zStream = new CASUALApp().getClass().getResourceAsStream(zipResource);
+        unZipInputStream(zStream, outputFolder);
+        zStream.close();
     }
     
     /**
-     * unzips a stream
+     * Unzips an InputStream.
+     * <p>
+     * Takes in an InputStream, converts it to a ZipInputStream, and then 
+     * iterates through all of the ZipEntries, writing each one of them to 
+     * a file with the name provided by the ZipEntry.
      * @param ZStream input stream to unzip
      * @param outputFolder output folder to unzip to
      * @throws FileNotFoundException
      * @throws IOException 
+     * @see InputStream
+     * @see ZipEntry
+     * @see ZipInputStream
+     * @see ZipFile
      */
-    public static void unZipInputStream(InputStream ZStream, String outputFolder) throws FileNotFoundException, IOException {
+    public static void unZipInputStream(InputStream zStream, String outputFolder) throws FileNotFoundException, IOException {
 
         ZipInputStream ZipInput;
-        ZipInput = new ZipInputStream(ZStream);
+        ZipInput = new ZipInputStream(zStream);
         ZipEntry ZipEntryInstance;
         while ((ZipEntryInstance = ZipInput.getNextEntry()) != null) {
             new Log().level3Verbose("Unzipping " + ZipEntryInstance.getName());
@@ -192,11 +222,14 @@ public class Unzip {
             Destination.close();
         }
         ZipInput.close();
+        zStream.close();
         new Log().level3Verbose("Unzip Complete");
     }
 
     /**
-     * closes the zip file
+     * Closes the zip file
+     * <p>
+     * Should be called after all file operations have been completed in Unzip
      */
     public void close() {
         try {
@@ -207,7 +240,9 @@ public class Unzip {
     }
 
     /**
-     * deploys a file from a zip
+     * Deploys a single file from a zip.
+     * <p>
+     * Takes in an ZipEntry, and writes that single zip entry out to a folder
      * @param entry entry file to deploy
      * @param outputFolder folder to be deployed to
      * @return
@@ -223,7 +258,9 @@ public class Unzip {
     }
 
     /**
-     * gets a stream of a specified file from a zip
+     * Gets a stream of a specified file from a zip.
+     * <p>
+     * Static method used to stream a file form a zip that is not an Unzip object.
      * @param zipFile file to stream from
      * @param entry entry to stream
      * @return stream of file
@@ -255,17 +292,26 @@ public class Unzip {
         is.close();
     }
     /**
-     * gets a stream of a specified file from a zip
-     * @param zipFile file to stream from
-     * @param entry entry to stream
-     * @return stream of file
+     * Retrieves a BufferedInputStream for a specific zip entry in a file. 
+     * @param entry ZipEntry that is to be pulled from ZipFile to Stream.
+     * @return BufferedInputStream of the specified ZipEntry.
      * @throws ZipException
      * @throws IOException
+     * @see ZipEntry
+     * @see ZipFile
+     * @see BufferedInputStream
      */
-    public BufferedInputStream streamFileFromZip( Object entry) throws ZipException, IOException {
+    public BufferedInputStream streamFileFromZip(Object entry) throws ZipException, IOException {
         return new BufferedInputStream(zip.getInputStream((ZipEntry) entry));
     }
     
+    /**
+     * Takes in a ZipEntry as an object and returns the string of the corresponding
+     * file name for the entry.
+     * @param entry the ZipEntry Object 
+     * @return name of the file contained in the 
+     * @see ZipEntry#getName() 
+     */
     public String getEntryName(Object entry){
         ZipEntry zipEntry = (ZipEntry)entry;
         String name=zipEntry.getName();
@@ -273,14 +319,14 @@ public class Unzip {
         
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    /**
+     * Takes in an ZipEntry as an object and returns the ZipEntry for the 
+     * Object.
+     * @param entry the ZipEntry Object
+     * @return the ZipEntry
+     * @see ZipEntry
+     */
+    public ZipEntry getEntry(Object entry) {
+        return (ZipEntry)entry;
+    }   
 }
