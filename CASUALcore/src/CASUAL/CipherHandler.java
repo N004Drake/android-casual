aa30a92a96ab8fd13dbf7d6c07ef5772610ecfb6
adamoutler <adamoutler@gmail.com>
2013-07-22 23:19:53 +0000

    adding PBKDF2 + HMAC SHA1 to encryption.

--- CASUALcore/src/CASUAL/CipherHandler.java
+++ CASUALcore/src/CASUAL/CipherHandler.java
@@ -1,6 +1,18 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/*CipherHandler provides a way to encrypt and decrypt given a password
+ *Copyright (C) 2013  Adam Outler
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package CASUAL;
 
@@ -9,15 +21,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-import java.security.MessageDigest;
+import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.KeySpec;
-import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.crypto.Cipher;
@@ -31,10 +41,10 @@ import javax.crypto.spec.SecretKeySpec;
 /**
  *
  * @author adam
+ * inspired by http://stackoverflow.com/questions/8674018/pbkdf2-with-bouncycastle-in-java
  * inspired by https://www.cigital.com/justice-league-blog/2009/08/14/proper-use-of-javas-securerandom/
  * inspired by http://stackoverflow.com/questions/1220751/how-to-choose-an-aes-encryption-mode-cbc-ecb-ctr-ocb-cfb
- * inspired by http://stackoverflow.com/questions/5531455/how-to-encode-some-string-with-sha256-in-java
- * inspired by Pulser
+ * slapped on the wrist several times by Pulser
  */
 public class CipherHandler {
     final File targetFile;
@@ -43,6 +53,12 @@ public class CipherHandler {
     }
     
     
+    /**
+     * encrypts a file to the output file
+     * @param output string location of file output
+     * @param key password
+     * @return modified version of password with random digits at the end, null if error
+     */
     public String encrypt(String output, String key){
         new Log().level2Information("Encrypting "+targetFile.getName());
 
@@ -58,6 +74,12 @@ public class CipherHandler {
         //key is returned.
         return key;
     }
+    /**
+     * decrypts a file
+     * @param output string name of file to output
+     * @param key password issued by encrytper
+     * @return name of file written, null if error
+     */
     public String decrypt(String output, String key){
         new Log().level2Information("Decrytping "+targetFile.getName());
         //filename is returned
@@ -110,14 +132,18 @@ public class CipherHandler {
         return retval;
     }
     
+    /**
+     * provides a one-way hash on a password
+     * @param input your password
+     * @return PBKDF2 with HMAC SHA1 password
+     */
     public byte[] oneWayHash(String input){
         try {
-            MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(input.getBytes("UTF-8"));
-            return hash;
-        } catch (NoSuchAlgorithmException ex) {
-            Logger.getLogger(CipherHandler.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (UnsupportedEncodingException ex) {
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+            KeySpec keyspec = new PBEKeySpec("password".toCharArray(), input.substring(input.length()-16).getBytes(), 1000, 256);
+            Key key = factory.generateSecret(keyspec);
+            return key.getEncoded();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
             Logger.getLogger(CipherHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
