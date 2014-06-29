e823597af23e241c3a755b6e9d4c5cd58712ee76
adamoutler <adamoutler@gmail.com>
2014-06-29 19:43:56 +0000

    faster preloader loading and more smooth animations

--- X/casualpreloader/src/casualpreloader/CasualPreloader.java
+++ X/casualpreloader/src/casualpreloader/CasualPreloader.java
@@ -18,6 +18,7 @@ package casualpreloader;
  * 
  * 
  */
+
 import com.sun.javaws.progress.Progress;
 import javafx.animation.FadeTransition;
 import javafx.animation.ScaleTransition;
@@ -35,114 +36,103 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextBoundsType;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
- import javafx.application.Preloader;
+import javafx.application.Preloader;
 import javafx.application.Preloader.ProgressNotification;
 import javafx.application.Preloader.StateChangeNotification;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.util.Duration;
+
 /**
- * Modified from Oracle JavaFX Ensemble Demo.
- * This is a splash
+ * Modified from Oracle JavaFX Ensemble Demo. This is a splash
  */
-public class CasualPreloader extends Preloader{
+public class CasualPreloader extends Preloader {
+
     //variables for storing initial position of the stage at the beginning of drag
-    Stage stage;
+    final int FADEOUTTIME = 7000;
     ProgressBar bar;
     Text text;
-    private void init(Stage primaryStage) {
-                
-                //create stage which has set stage style transparent
-                 stage = new Stage(StageStyle.TRANSPARENT);
-                //create root node of scene, i.e. group
-                Group rootGroup = new Group();
-                //create scene with set width, height and color
-                Scene scene = new Scene(rootGroup, 300, 300, Color.TRANSPARENT);
-                //set scene to stage
-                stage.setScene(scene);
-                //center stage on screen
-                stage.centerOnScreen();
-                //show the stage
-                stage.show();
-                ProgressIndicator pi = new ProgressIndicator();
-                pi.setProgress(-.1);
-                pi.setMinWidth(240);
-                pi.setMinHeight(240);
-                pi.setStyle("-fx-progress-color: darkgray");
-                pi.setOpacity(.1);
-                bar=new ProgressBar();
-                bar.setStyle("-fx-accent: green;");
-                bar.setScaleY(2);
-                // CREATE SIMPLE TEXT NODE
-                text = new Text("CASUAL"); //20, 110,
-                text.setFill(Color.WHITESMOKE);
-                text.setEffect(new Lighting());
-                text.setBoundsType(TextBoundsType.VISUAL);
-                text.setFont(Font.font(Font.getDefault().getFamily(), 30));
- 
-                // USE A LAYOUT VBOX FOR EASIER POSITIONING OF THE VISUAL NODES ON SCENE
-                VBox vBox = new VBox();
-                vBox.setSpacing(10);
-                vBox.setPadding(new Insets(90, 0, 0, 60));
-                vBox.setAlignment(Pos.TOP_CENTER);
-                vBox.getChildren().addAll(text,bar);
-                
-                //add all nodes to main root group
-                rootGroup.getChildren().addAll(vBox,pi);
-    }
- 
-    @Override 
+    FadeTransition finalFadeOut;
+
+    @Override
     public void start(Stage primaryStage) throws Exception {
-        init(primaryStage);
-        //primaryStage.show();
-     
+        //create stage which has set stage style transparent
+        primaryStage = new Stage(StageStyle.TRANSPARENT);
+        this.init(primaryStage);
+
     }
-    @Override
-    public void handleStateChangeNotification(StateChangeNotification scn) {
-        if (scn.getType() == StateChangeNotification.Type.BEFORE_START) {
-             FadeTransition ft = new FadeTransition(
-                    Duration.millis(10000), stage.getScene().getRoot());
-            ft.setFromValue(1.0);
-            ft.setToValue(0.0);
-            final Stage s = stage;
-            EventHandler<ActionEvent> eh = new EventHandler<ActionEvent>() {
-                public void handle(ActionEvent t) {
-                    s.hide();
-                    s.close();
-                }
-            };
-            ft.setOnFinished(eh);
-            ft.play();
-                
-        }
+
+    private void init(final Stage primaryStage) {
+
+        //create root node of scene, i.e. group
+        Group rootGroup = new Group();
+        //create scene with set width, height and color
+        Scene scene = new Scene(rootGroup, 300, 300, Color.TRANSPARENT);
+        //set scene to stage
+        primaryStage.setScene(scene);
+        //center stage on screen
+        primaryStage.centerOnScreen();
+
+        ProgressIndicator pi = new ProgressIndicator();
+        pi.setProgress(-.1);
+        pi.setMinWidth(240);
+        pi.setMinHeight(240);
+        pi.setStyle("-fx-progress-color: darkgray");
+        pi.setOpacity(.1);
+        bar = new ProgressBar();
+        bar.setStyle("-fx-accent: green;");
+        bar.setScaleY(2);
+        // CREATE SIMPLE TEXT NODE
+        text = new Text("CASUAL"); //20, 110,
+        text.setFill(Color.WHITESMOKE);
+        text.setEffect(new Lighting());
+        text.setBoundsType(TextBoundsType.VISUAL);
+        text.setFont(Font.font(Font.getDefault().getFamily(), 30));
+
+        // USE A LAYOUT VBOX FOR EASIER POSITIONING OF THE VISUAL NODES ON SCENE
+        VBox vBox = new VBox();
+        vBox.setSpacing(10);
+        vBox.setPadding(new Insets(90, 0, 0, 60));
+        vBox.setAlignment(Pos.TOP_CENTER);
+        vBox.getChildren().addAll(text, bar);
+
+        rootGroup.getChildren().addAll(vBox, pi);
+        //add all nodes to main root group
+        //show the stage
+        primaryStage.show();
+        this.finalFadeOut = new FadeTransition(Duration.millis((double) 7000.0), primaryStage.getScene().getRoot());
+        this.finalFadeOut.setFromValue(1.0);
+        this.finalFadeOut.setToValue(0.0);
+        EventHandler<ActionEvent> eh = new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent t) {
+                primaryStage.hide();
+                primaryStage.close();
+            }
+        };
+        finalFadeOut.setOnFinished(eh);
+
     }
-    
-    boolean finalnotification=false;
+
     @Override
-    public void handleProgressNotification(ProgressNotification pn) {
-        bar.setProgress(pn.getProgress());
-        if (pn.getProgress()>.98 && finalnotification==false){
-            finalnotification=true;
-            FadeTransition ft = new FadeTransition(
-                    Duration.millis(1000),bar);
-            ft.setFromValue(1.0);
-            ft.setToValue(0.0);
-            final Stage s = stage;
-            EventHandler<ActionEvent> eh = new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent t) {
-                    text.setText(text.getText()+"\npreparing");
-                    ScaleTransition st = new ScaleTransition(Duration.millis(1000),text);
-                    st.setFromX(1);
-                    st.setToX(1.5);
-                    st.setFromY(1);
-                    st.setToY(1.5);
-                    st.play();
-                }
-            };
-            ft.setOnFinished(eh);
-            ft.play();
+    public void handleStateChangeNotification(Preloader.StateChangeNotification scn) {
+        if (scn.getType() != Preloader.StateChangeNotification.Type.BEFORE_START) {
+            return;
         }
-    }   
-    public static void main(String[] args) { launch(args); }
+        this.finalFadeOut.play();
+    }
+
+    @Override
+    public void handleProgressNotification(Preloader.ProgressNotification pn) {
+
+        CasualPreloader.this.bar.setProgress(pn.getProgress());
+        if (pn.getProgress() <= 0.98) {
+            return;
+        }
+        CasualPreloader.this.text.setText(CasualPreloader.this.text.getText() + "\npreparing");
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
