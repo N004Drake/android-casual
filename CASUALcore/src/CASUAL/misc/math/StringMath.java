016f9521caa2ccf347b24477e60dece4f6744d6f
adamoutler <adamoutler@gmail.com>
2014-11-28 16:15:29 +0000

    Adding StringMath class with test.  Feed it a string and it will evaluate it as Javascript.

---
+++ CASUALcore/src/CASUAL/misc/math/StringMath.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2014 adamoutler
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package CASUAL.misc.math;
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+/**
+ *
+ * @author adamoutler
+ */
+public class StringMath {
+   ScriptEngine engine = new ScriptEngineManager().getEngineByName("JavaScript");
+        
+    public String performRoundedMathOperation(String mathProblem) throws CASUALMathOperationException{
+        try {
+            String s=engine.eval(mathProblem).toString();
+            s = !s.contains(".") ? s : s.replaceAll("0*$", "").replaceAll("\\.$", "");
+            return s;
+        } catch (ScriptException ex) {
+            try {
+                throw new CASUALMathOperationException(mathProblem+ " evaluated to: "+engine.eval(mathProblem));
+            } catch (ScriptException ex1) {
+                throw new CASUALMathOperationException(mathProblem+ " could not be evaluated");
+            }
+        }
+
+    
+    }
+}
